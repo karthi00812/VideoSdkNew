ffff556e76db6a908ed685b2a243b6b51676c425
karthik.annadurai <askarthikannadurai@gmail.com>
2024-09-02 17:12:01 +0530

    bug fix for socket connect and disconnect.

--- public/wssAgent.js
+++ public/wssAgent.js
@@ -2,6 +2,7 @@ import * as store from "./store.js";
 import * as webRTCHandler from "./RTCHandlerAgent.js";
 import * as constants from "./constants.js";
 import * as ui from "./uiInteract.js";
+import * as sessionTool from "./agent.js";
 
 let socketIO = null;
 
@@ -9,9 +10,16 @@ export const registerSocketEvents = (socket) => {
   socketIO = socket;
 
   socket.on("emitUser", (msg) => {
-    console.log(msg);
+    console.log("socket connected emitUser" + JSON.stringify(msg));
     store.setSocketId(msg.id.connection_id, msg.id.user);
     ui.updatePersonalCode(msg.id.user);
+    const connect_vc = document.querySelector("#connect_vc");
+    connect_vc.classList.remove("btn-secondary");
+    connect_vc.classList.add("btn-success");
+    connect_vc.textContent = "Disconnect VC";
+    connect_vc.dataset.status = "connected";
+    document.querySelector("#status").textContent = "VC Connected";
+    sessionTool.session.start();
   });
 
   socket.on("pre-offer", (data) => {
@@ -34,21 +42,24 @@ export const registerSocketEvents = (socket) => {
   let ringtone = new Audio("./audio/user_disconnect.mp3");
   socket.on("disconnect", () => {
 
+    ringtone.play();
     const status_bar = document.querySelector("#status_bar");
     status_bar.style.display = "flex";
-    ringtone.play();
-    ringtone.pause();
     document.querySelector("#user_id").textContent = "####";
     const connect_vc = document.querySelector("#connect_vc")
     connect_vc.classList.add("btn-secondary");
     connect_vc.classList.remove("btn-success");
     connect_vc.textContent = "Connect VC";
     connect_vc.dataset.status = "disconnected";
+    console.log("socket disconnected");
+    ringtone.pause();
+    sessionTool.session.dispose();
   });
 
   socket.on("connect", () => {
     const status_bar = document.querySelector("#status_bar");
     status_bar.style.display = "none";
+    console.log("socket connected");
   })
 
   socket.on("webRTC-signaling", (data) => {
